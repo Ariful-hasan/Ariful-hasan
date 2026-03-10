93f16c3bca8d6f819f543935284f946c46843eac
Ariful Hasan <arifhasan.mbstu@gmail.com>
2026-03-10 21:30:48 +0100

    add base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ariful-hasan.github.io',
+  base: '/Ariful-Hasan',
   integrations: [react()],
 
   vite: {
